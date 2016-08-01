0e8f6b7be7f400f8f0f434f985315bb777733f7e
Priyanka R <priyanka.june15@gmail.com>
2016-08-01 10:43:43 -0700

    Second commit

--- app/src/main/java/yezhenwang/newgenerationspotify/MainPageActivity.java
+++ app/src/main/java/yezhenwang/newgenerationspotify/MainPageActivity.java
@@ -2,34 +2,27 @@ package yezhenwang.newgenerationspotify;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-<<<<<<< HEAD
 import android.widget.ImageView;
-=======
->>>>>>> 6364af332a4b9630d4b3021abe991e5ad82b1c3a
+
 import android.widget.TextView;
 
 public class MainPageActivity extends AppCompatActivity {
 
-<<<<<<< HEAD
 
     TextView greetings;
     ImageView imageView;
 
 
-=======
-<<<<<<< HEAD
+
     ArtistPicFragment artistPicFragment;
 
-=======
->>>>>>> 92d146395969b4e0906412cb729b5da00aec5de0
+
     TextView greetings;
->>>>>>> 6364af332a4b9630d4b3021abe991e5ad82b1c3a
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_page);
-<<<<<<< HEAD
         imageView = (ImageView) findViewById(R.id.imageView);
 
 
